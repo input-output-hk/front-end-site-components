15da1f457ca8da9c724831b7c58c1c853c1531b9
George Clark <clarkgeorge76@gmail.com>
2020-04-23 10:33:41 +0100

    add new footer mentioning daedalus

---
+++ src/footers/Daedalus.js
@@ -0,0 +1,235 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { FaTwitter, FaFacebookF, FaYoutube, FaRss } from 'react-icons/fa'
+import { DiGithubBadge } from 'react-icons/di'
+import Markdown from '@input-output-hk/front-end-core-components/components/Markdown'
+import Image from '@input-output-hk/front-end-core-components/components/Image'
+import Link from '@input-output-hk/front-end-core-components/components/Link'
+import daedalus from '../resources/footer/daedalus'
+
+const Container = styled.div`
+  margin: 3rem 0;
+`
+
+const TopRow = styled.div`
+  display: flex;
+
+  > div {
+    flex: 1;
+
+    &:last-of-type {
+      flex: 1.5;
+    }
+  }
+
+  @media screen and (max-width: 700px) {
+    flex-direction: column;
+  }
+`
+
+const BottomRow = styled.div`
+  display: flex;
+
+  > div {
+    flex: 1;
+
+    &:first-of-type {
+      flex: 1.1;
+      margin-right: 1rem;
+    }
+
+    &:last-of-type {
+      display: flex;
+      margin-left: 1rem;
+    }
+  }
+
+  @media screen and (max-width: 900px) {
+    flex-direction: column;
+
+    > div {
+      &:first-of-type,
+      &:last-of-type {
+        margin: 0;
+      }
+    }
+  }
+
+  @media screen and (max-width: 400px) {
+    > div:last-of-type {
+      flex-direction: column;
+    }
+  }
+`
+
+const Copyright = styled.div`
+  display: flex;
+  
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    > a {
+      margin-top: 0.2rem;
+    }
+
+    &:first-of-type {
+      margin-right: 1rem;
+    }
+  }
+`
+
+const TopRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  @media screen and (max-width: 700px) {
+    justify-content: flex-start;
+  }
+
+  @media screen and (max-width: 400px) {
+    flex-direction: column;
+  }
+`
+
+const Logo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-right: 2rem;
+  flex-direction: row;
+
+  img {
+    width: 5rem;
+  }
+
+  > a {
+    display: flex;
+    > div {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      &:last-of-type {
+        padding-left: 1rem;
+        flex-grow: 1;
+      }
+    }
+  }
+
+  @media screen and (max-width: 400px) {
+    justify-content: flex-start;
+    margin-bottom: 1rem;
+  }
+`
+
+const SocialLinks = styled.div`
+  display: flex;
+`
+
+const SocialLink = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 1rem;
+`
+
+const LinksColumn = styled.div`
+  flex: 1;
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 1rem 0 0;
+  }
+`
+
+const Daedalus = ({ lang, theme }) => {
+  const content = daedalus[lang]
+  if (!content) throw new Error(`No content for Cardano footer in language "${lang}"`)
+  let logoURL = 'https://ucarecdn.com/75b74f03-ff04-47ba-821c-5e477d3d46d4/'
+  if (theme === 'dark') logoURL = 'https://ucarecdn.com/8102d054-1169-4af8-81a9-1ea43c680a45/'
+  return (
+    <Container>
+      <TopRow>
+        <Copyright>
+          <div>
+            <p>© Daedalus Wallet 2015 - {new Date().getFullYear()}</p>
+          </div>
+          <div>
+            <Link rel='noopener' title='Input Output HK GitHub' href='https://github.com/input-output-hk'>
+              <DiGithubBadge size={25} />
+            </Link>
+          </div>
+        </Copyright>
+        <TopRight>
+          <Logo>
+            <Link rel='noopener' href='https://iohk.io/' title='Input Output HK'>
+              <div>
+                <Image src={logoURL} alt='IOHK logo' sizeFactor={0.14} maintainTransparency />
+              </div>
+              <div>
+                <p>IOHK supported project</p>
+              </div>
+            </Link>
+          </Logo>
+          <SocialLinks>
+            <SocialLink>
+              <Link title='Input Output HK Twitter' rel='noopener' href='https://twitter.com/inputoutputHK'>
+                <FaTwitter size={20} />
+              </Link>
+            </SocialLink>
+            <SocialLink>
+              <Link title='Input Output HK Facebook' rel='noopener' href='https://www.facebook.com/iohk.io/'>
+                <FaFacebookF size={20} />
+              </Link>
+            </SocialLink>
+            <SocialLink>
+              <Link title='Input Output HK YouTube' rel='noopener' href='https://www.youtube.com/c/IohkIo'>
+                <FaYoutube size={20} />
+              </Link>
+            </SocialLink>
+            <SocialLink>
+              <Link title='Input Output HK Blog' rel='noopener' href='https://iohk.io/blog/'>
+                <FaRss size={20} />
+              </Link>
+            </SocialLink>
+          </SocialLinks>
+        </TopRight>
+      </TopRow>
+      <hr />
+      <BottomRow>
+        <div className='body-text'>
+          <Markdown source={content.body} />
+        </div>
+        <div>
+          <LinksColumn>
+            <p><strong>{content.cardanoLinks.title}</strong></p>
+            <ul>
+              {content.cardanoLinks.links.map(({ href, label }) => (
+                <li key={`${href}_${label}`}><Link href={href} rel='noopener'>{label}</Link></li>
+              ))}
+            </ul>
+          </LinksColumn>
+          <LinksColumn>
+            <p><strong>{content.communityLinks.title}</strong></p>
+            <ul>
+              {content.communityLinks.links.map(({ href, label }) => (
+                <li key={`${href}_${label}`}><Link href={href} rel='noopener'>{label}</Link></li>
+              ))}
+            </ul>
+          </LinksColumn>
+        </div>
+      </BottomRow>
+    </Container>
+  )
+}
+
+Daedalus.propTypes = {
+  lang: PropTypes.string.isRequired,
+  theme: PropTypes.oneOf([ 'dark', 'light' ]).isRequired
+}
+
+export default Daedalus
---
+++ src/resources/footer/daedalus.js
@@ -0,0 +1,258 @@
+export default {
+  en: {
+    body: `
+Daedalus is an open source project.
+
+Cardano is a software platform ONLY and does not conduct any independent diligence on, or substantive review of, any blockchain asset, digital currency, cryptocurrency or associated funds. You are fully and solely responsible for evaluating your investments, for determining whether you will exchange blockchain assets based on your own judgment, and for all your decisions as to whether to exchange blockchain assets with Cardano. In many cases, blockchain assets you exchange on the basis of your research may not increase in value, and may decrease in value. Similarly, blockchain assets you exchange on the basis of your research may fall or rise in value after your exchange.
+
+_Past performance is not indicative of future results. Any investment in blockchain assets involves the risk of loss of part or all of your investment. The value of the blockchain assets you exchange is subject to market and other investment risks_
+    `,
+    cardanoLinks: {
+      title: 'More about Cardano',
+      links: [
+        { label: 'Cardano Explorer', href: 'https://cardanoexplorer.com/' },
+        { label: 'Cardano Foundation', href: 'https://cardanofoundation.org/' },
+        { label: 'Documentation', href: 'https://cardanodocs.com/introduction/' },
+        { label: 'Cardano SL Source', href: 'https://github.com/input-output-hk/cardano-sl' },
+        { label: 'Cardano.org', href: 'https://www.cardano.org/' },
+        { label: 'Why Cardano', href: 'https://whycardano.com/' },
+        { label: 'Daedalus', href: 'https://daedaluswallet.io/' },
+        { label: 'Cardano Testnets', href: 'https://testnet.iohkdev.io/' },
+        { label: 'Cardano Rust', href: 'https://cardanorust.iohkdev.io/' }
+      ]
+    },
+    communityLinks: {
+      title: 'Join the community',
+      links: [
+        { label: 'Cardano Community', href: 'https://cardano.org/' },
+        { label: 'Cardano Telegram', href: 'https://t.me/CardanoAnnouncements/' },
+        { label: 'Cardano Forum', href: 'https://forum.cardano.org/' },
+        { label: 'Cardano Reddit', href: 'https://www.reddit.com/r/cardano/' },
+        { label: 'IOHK', href: 'https://www.iohk.io/' },
+        { label: 'IOHK blog', href: 'https://www.iohk.io/blog/' },
+        { label: 'IOHK YouTube', href: 'https://www.youtube.com/channel/UCBJ0p9aCW-W82TwNM-z3V2w' },
+        { label: 'Cardano Foundation YouTube', href: 'https://www.youtube.com/channel/UCbQ9vGfezru1YRI1zDCtTGg' },
+        { label: 'Cardano Foundation Twitter', href: 'https://twitter.com/cardanostiftung' }
+      ]
+    }
+  },
+  ja: {
+    body: `
+Daedalusはオープンソースプロジェクトです。
+
+Cardanoはソフトウェアプラットフォームのみを提供します。いかなるブロックチェーン資産、デジタル通貨、暗号通貨または関連資産についても、独立したデューディリジェンスや実質的なレビューを行うことはありません。投資者は、自身の投資評価、自己判断に基くブロックチェーン資産取引きを行うかどうかの決定、Cardanoでブロックチェーン資産取引きを行うかどうかの決定について、全面的に単独で責任を負います。
+
+投資調査に基づいて取引きされるブロックチェーン資産の価値が増加しない、または減少することはよくあります。同様に、投資調査に基づいて取引きするブロックチェーン資産の価格は、取引き後に上昇する場合も下落する場合もあります。
+    `,
+    cardanoLinks: {
+      title: 'Cardanoについてもっと知る',
+      links: [
+        { label: 'Cardanoエクスプローラー', href: 'https://cardanoexplorer.com/' },
+        { label: 'Cardano財団', href: 'https://cardanofoundation.org/' },
+        { label: 'ドキュメンテーション', href: 'https://cardanodocs.com/introduction/' },
+        { label: 'Cardano SLソース', href: 'https://github.com/input-output-hk/cardano-sl' },
+        { label: 'Cardano.org', href: 'https://www.cardano.org/' },
+        { label: 'Daedalus', href: 'https://daedaluswallet.io' },
+        { label: 'Cardanoを選ぶ理由', href: 'https://whycardano.com/jp/' },
+        { label: 'Cardanoテストネット', href: 'https://testnet.iohkdev.io/' },
+        { label: 'Cardano Rust', href: 'https://cardanorust.iohkdev.io/' }
+      ]
+    },
+    communityLinks: {
+      title: 'コミュニティに参加する',
+      links: [
+        { label: 'Cardanoコミュニティ', href: 'https://www.cardano.org/ja/home-2/' },
+        { label: 'Cardano Telegram', href: 'https://t.me/CardanoJapanOfficial' },
+        { label: 'Cardanoフォーラム', href: 'https://forum.cardano.org/' },
+        { label: 'Cardano Reddit', href: 'https://www.reddit.com/r/cardano/' },
+        { label: 'IOHK', href: 'https://www.iohk.io/' },
+        { label: 'IOHK blog', href: 'https://www.iohk.io/blog/' },
+        { label: 'IOHK YouTube', href: 'https://www.youtube.com/channel/UCBJ0p9aCW-W82TwNM-z3V2w' },
+        { label: 'Cardano Foundation YouTube', href: 'https://www.youtube.com/channel/UCbQ9vGfezru1YRI1zDCtTGg' },
+        { label: 'Cardano Foundation Twitter', href: 'https://twitter.com/cardanostiftung' }
+      ]
+    }
+  },
+  ko: {
+    body: `
+Cardano는 오픈소스 프로젝트입니다.
+
+Cardano는 소프트웨어 플랫폼일 뿐이며 블록체인 자산, 디지털 통화, 암호화폐 또는 관련 자금에 대한 어떠한 독립적인 실사나 실질적인 검토를 수행하지 않습니다. 귀하의 투자에 대한 평가, 블록체인 자산을 교환할 것인지 여부는 귀하의 전적인 책임하게 있으며, 블록체인 자산을 Cardano에서 거래할지 여부에 대한 모든 결정에 대한 책임 또한 전적으로 귀하에게 있습니다.
+
+대부분의 경우, 귀하가 스스로 행한 연구를 근거로 블록체인 자산을 거래하더라도 블록체인 자산가치는 상승하지 않을 수 있으며, 오히려 가치가 하락할 수도 있습니다. 마찬가지로, 귀하가 스스로 행한 연구를 근거로 거래하는 블록체인 자산은 거래 후에 가치가 떨어지거나 상승할 수 있습니다.
+    `,
+    cardanoLinks: {
+      title: 'Cardano에 대한 추가 정보',
+      links: [
+        {
+          'href': 'https://cardanoexplorer.com/',
+          'label': 'Cardano 탐색기'
+        },
+        {
+          'href': 'https://cardanofoundation.org/',
+          'label': 'Cardano Foundation'
+        },
+        {
+          'href': 'https://cardanodocs.com/introduction/',
+          'label': '문서'
+        },
+        {
+          'href': 'https://github.com/input-output-hk/cardano-sl',
+          'label': 'Cardano SL source'
+        },
+        {
+          'href': 'https://whycardano.com/',
+          'label': '왜 Cardano인가'
+        },
+        {
+          'href': 'https://cardano.org',
+          'label': 'Cardano.org'
+        },
+        {
+          'href': 'https://cardaedaluswallet.io',
+          'label': 'Daedalus'
+        },
+        {
+          'href': 'https://testnet.iohkdev.io',
+          'label': 'Cardano 테스트넷'
+        },
+        {
+          'href': 'https://cardanorust.iohkdev.io',
+          'label': 'Cardano Rust'
+        }
+      ]
+    },
+    communityLinks: {
+      title: '커뮤니티 가입하기',
+      links: [
+        {
+          'href': 'https://cardano.org',
+          'label': 'Cardano 커뮤니티'
+        },
+        {
+          'href': 'https://t.me/CardanoAnnouncements/',
+          'label': 'Cardano 텔레그램'
+        },
+        {
+          'href': 'https://forum.cardano.org/',
+          'label': 'Cardano 포럼'
+        },
+        {
+          'href': 'https://www.reddit.com/r/cardano/',
+          'label': 'Cardano 레딧'
+        },
+        {
+          'href': 'https://iohk.io/',
+          'label': 'IOHK'
+        },
+        {
+          'href': 'https://iohk.io/blog/',
+          'label': 'IOHK 블로그'
+        },
+        {
+          'href': 'https://www.youtube.com/channel/UCBJ0p9aCW-W82TwNM-z3V2w',
+          'label': 'IOHK 유튜브'
+        },
+        {
+          'href': 'https://www.youtube.com/channel/UCbQ9vGfezru1YRI1zDCtTGg',
+          'label': 'Cardano Foundation YouTube'
+        },
+        {
+          'href': 'https://twitter.com/cardanostiftung',
+          'label': 'Cardano 재단 트위터'
+        }
+      ]
+    }
+  },
+  'zh-cn': {
+    body: `
+Cardano是一个开源项目。
+
+Cardano只是一种软件平台，不对任何区块链资产、数字货币、加密货币或相关资金进行任何独立的调查或实质性审查。您对您的投资评估、根据自己的判断交换区块链资产以及是否用Cardano交换区块链资产的所有决定负完全和唯一的责任。
+
+在许多情况下，您根据自己的研究而进行交换的区块链资产可能不会增值，甚至可能会减值。同样，您根据研究交换的区块链资产的价值在交换后可能会减少或增加。
+    `,
+    cardanoLinks: {
+      title: '关于Cardano的更多信息',
+      links: [
+        {
+          'href': 'https://cardanoexplorer.com/',
+          'label': 'Cardano浏览器'
+        },
+        {
+          'href': 'https://cardanofoundation.org/',
+          'label': 'Cardano Foundation'
+        },
+        {
+          'href': 'https://cardanodocs.com/introduction/cn/',
+          'label': '文档'
+        },
+        {
+          'href': 'https://github.com/input-output-hk/cardano-sl',
+          'label': 'Cardano SL来源'
+        },
+        {
+          'href': 'https://whycardano.com/cn/',
+          'label': '为何选择Cardano'
+        },
+        {
+          'href': 'https://cardano.org',
+          'label': 'Cardano.org'
+        },
+        {
+          'href': 'https://daedaluswallet.io',
+          'label': 'Daedalus'
+        },
+        {
+          'href': 'https://testnet.iohkdev.io',
+          'label': 'Cardano Testnets'
+        },
+        {
+          'href': 'https://cardanorust.iohkdev.io',
+          'label': 'Cardano Rust'
+        }
+      ]
+    },
+    communityLinks: {
+      title: '加入社区',
+      links: [
+        {
+          'href': 'https://www.cardano.org/zh/home-3/',
+          'label': 'Cardano社区'
+        },
+        {
+          'href': 'https://t.me/CardanoAnnouncements/',
+          'label': 'Cardano Telegram'
+        },
+        {
+          'href': 'https://forum.cardano.org/',
+          'label': 'Cardano论坛'
+        },
+        {
+          'href': 'https://www.reddit.com/r/cardano/',
+          'label': 'Cardano Reddit'
+        },
+        {
+          'href': 'https://iohk.io',
+          'label': 'IOHK'
+        },
+        {
+          'href': 'https://iohk.io/blog',
+          'label': 'IOHK博客'
+        },
+        {
+          'href': 'https://www.youtube.com/channel/UCBJ0p9aCW-W82TwNM-z3V2w',
+          'label': 'IOHK YouTube'
+        },
+        {
+          'href': 'https://www.youtube.com/channel/UCbQ9vGfezru1YRI1zDCtTGg',
+          'label': 'Cardano Foundation YouTube'
+        },
+        {
+          'href': 'https://twitter.com/cardanostiftung',
+          'label': 'Cardano Foundation Twitter'
+        }
+      ]
+    }
+  }
+}
